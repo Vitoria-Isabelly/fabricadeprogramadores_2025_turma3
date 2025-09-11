2a0e327e6b65e716534b2d79691f7309b999d35e
vitoria <vitoriapython02@gmail.com>
2025-09-11 15:35:53 -0300

    Add files via upload

---
+++ app.py
@@ -0,0 +1,62 @@
+from flask import Flask, request, jsonify
+import sqlite3
+
+app = Flask(__name__)
+
+BANCO_DE_DADOS = "bloco_de_Notas.db"
+
+def get_db():
+    db = sqlite3.connect(BANCO_DE_DADOS)
+    db.row_factory = sqlite3.Row
+    return db
+
+def init_db():
+    with app.app_db():
+        db = get_db()
+        with app.open_resource('schema.sql', mode = 'r') as f:
+            db.cursor().execultescript(f.read())
+        db.commit()
+
+def new_note(nota):
+    con = sqlite3.connect(BANCO_DE_DADOS)
+    cur = con.cursor()
+    cur.execute("""
+            INSERT INTO anotacoes (texto, data_hora)
+            VALUES(%s, datetime('now'))
+            """ % nota)
+    con.commit()
+    cur.close()
+    con.close()
+
+@app.route('/iniciar')
+def iniciar():
+    init_db()
+    return 'Banco de dados iniciado...'
+
+@app.route('/home', methods=["GET"])
+def home():
+    try:
+            db = get_db()
+            cursor = db.cursor()
+            cursor.execute('SELECT FROM anotacoes')
+            dados = cursor.fetchall()
+            return jsonify([dict(row) for row in dados])
+    except sqlite3. Error as e:
+            return jsonify({'error': str(e)}), 500 
+    finally:
+            db.close()
+
+@app.route('/')
+def index():
+    return '''<h1>Bloco de Notas</h1>
+            <h2>Rotas:</h2>
+            <ul>
+                <li> 
+                    <a href="/iniciar">/iniciar</a>
+                </li>
+                <li>
+                    <a href="/home">/home</a>
+                </li>
+            </ul>
+    '''
+
