7e56c42771c6341ff5eb0210432fe9a1b0f8888c
vitoria <vitoriapython02@gmail.com>
2025-09-19 15:16:14 -0300

    Add files via upload

--- main.py
+++ main.py
@@ -1,32 +1,56 @@
-from animais_marinhos import AnimalMarinho
+from tabelas import SessionLocal, Usuario, Nota
 
-animais = [
-    AnimalMarinho("Golfinho", "Delphinus delphis", 2.5, True, "oceano"),
-    AnimalMarinho("Tubarão Branco", "Carcharodon carcharias", 6.0, True, "oceano"),
-    AnimalMarinho("Baleia Azul", "Balaenoptera musculus", 24.0, False, "oceano"),
-    AnimalMarinho("Cavalo-Marinho", "Hippocampus", 0.1, False, "recifes"),
-    AnimalMarinho("Polvo", "Octopus vulgaris", 1.2, True, "fundo do mar"),
-    AnimalMarinho("Estrela-do-mar", "Asteroidea", 0.3, False, "recifes"),
-    AnimalMarinho("Orca", "Orcinus orca", 8.0, True, "oceano"),
-    AnimalMarinho("Lula Gigante", "Architeuthis", 12.0, True, "profundezas do oceano"),
-    AnimalMarinho("Foca", "Phocidae", 1.8, True, "águas geladas"),
-    AnimalMarinho("Leão-marinho", "Otariinae", 2.4, True, "costa"),
-    AnimalMarinho("Caranguejo", "Brachyura", 0.2, False, "praias rochosas"),
-    AnimalMarinho("Tartaruga Marinha", "Cheloniidae", 1.5, False, "oceano"),
-    AnimalMarinho("Peixe-palhaço", "Amphiprioninae", 0.2, False, "recifes de coral"),
-    AnimalMarinho("Narval", "Monodon monoceros", 4.5, True, "águas árticas"),
-    AnimalMarinho("Peixe-boi", "Trichechus", 3.0, False, "rios costeiros"),
-    AnimalMarinho("Água-viva", "Cnidaria", 0.5, False, "mar aberto"),
-    AnimalMarinho("Enguia", "Anguilliformes", 1.0, True, "rios e mares"),
-    AnimalMarinho("Raia", "Batoidea", 2.5, True, "fundo do mar"),
-    AnimalMarinho("Coral", "Anthozoa", 0.05, False, "recifes"),
-    AnimalMarinho("Linguado", "Pleuronectiformes", 0.4, True, "fundo do mar"),
-]
+db = SessionLocal()
 
-for animal in animais:
-    animal.exibir_dados()
-    animal.emitir_som()
-    animal.nadar()
-    animal.alimentar()
-    animal.crescer(0.1)
-    print("-" * 40)
+def criar_novo_usuario_e_nota():
+    """Exemplo de como CRIAR dados."""
+    novo_usuario = Usuario( 
+        nome="Vitória Isabelly",
+        email="Vitoriaisabelly21107@email.com",
+        senha_hash="hash_super_seguro"
+    )  
+
+    db.add(novo_usuario) 
+    db.commit()
+
+    print(f"Usuário '{novo_usuario.nome}' criado com ID: {novo_usuario.id}")
+
+    nova_nota = Nota(
+        titulo="Minha Primeira Nota com SQLAlchemy",
+        conteudo="É muito mais fácil do que escrever SQL na mão!",
+        autor = novo_usuario
+    )
+
+    db.add(nova_nota)
+    db.commit()
+    print (f"Nota '{nova_nota.titulo}' criada para {novo_usuario.nome}.")
+
+def ler_dados (nome):
+    """Exemplo de como LER dados."""
+
+    user = db.query(Usuario).filter(Usuario.nome == nome).first()
+
+    if user:
+        print(f"Encontrei o(a): {user.nome} (Email: {user.email})")
+    
+        print("Notas do user:")
+        for nota in user.notas :
+            print(f" - Título: {nota.titulo} (ID: {nota.id})")
+    else:
+        print("Usuário(a) não encontrado.")
+
+def atualizar_nota(id_nota):
+    """Exemplo de como ATUALIZAR dados."""
+
+    nota_para_editar = db.query(Nota).filter(Nota.id == id_nota).first()
+
+    if nota_para_editar:
+        print(f"Título original: '{nota_para_editar.titulo}'")
+
+        nota_para_editar.titulo = "Lista de anotações ATULIZADA!"
+
+        db.commit()
+        print(f"Título novo: '{nota_para_editar.titulo}'")
+
+    else:
+        print("Nota com ID %d nçao encontrada." % id_nota)
